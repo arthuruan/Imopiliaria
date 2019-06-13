f087a8dc59835b5ff887a3c4aacbdddf954d2b15
unknown <arthur123ruan@hotmail.com>
2019-06-13 14:55:59 -0300

    començando

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+#define CLEAR system(cls);
+
+void menu1(){
+
+
+    return;
+}
+
 int main(void){
+
+    switch (menu1()){
+
+    case /* constant-expression */:
+        /* code*/
+        break;
     
+    default:
+        break;
+    }
     return 0;
 }
