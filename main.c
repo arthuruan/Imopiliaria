72ff7617fda16403c575d9e49c34ab05caaefd6e
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2019-06-29 00:36:32 -0300

    função lerArquivoImovel(em andamento).

--- main.c
+++ main.c
@@ -373,6 +373,59 @@ void BuscaTitulo(){
     PAUSE;
 }
 
+//ler todas as informações do vetor imovel contidas no arquivo .txt
+void lerArquivoImovel(int flag) {
+	FILE* arquivo;
+	int i;
+
+	arquivo = fopen("dadosImovel.txt", "r");
+	if (arquivo <= 0) {
+		puts("!!!Erro ao abrir o arquivo!!!");
+		return;
+	}
+
+	for (i = 0; i < 100; i++) {
+		if (feof(arquivo))
+			break;
+
+		//ler cadastro
+		fgets(imovel[i].cidade, 100, arquivo);
+		imovel[i].cidade[strlen(imovel[i].cidade) - 1] = '\0';
+		fgets(imovel[i].bairro, 50, arquivo);
+		imovel[i].bairro[strlen(imovel[i].bairro) - 1] = '\0';
+		fgets(imovel[i].endereco, 100, arquivo);
+		imovel[i].endereco[strlen(imovel[i].endereco) - 1] = '\0';
+		fgets(imovel[i].cep, 15, arquivo);
+		imovel[i].cep[strlen(imovel[i].cep) - 1] = '\0';
+		fscanf(arquivo, "%f\n", &imovel[i].valor);
+		fgets(imovel[i].disponivel, 10, arquivo);
+		imovel[i].disponivel[strlen(imovel[i].disponivel) - 1] = '\0';
+		fgets(imovel[i].titulo, 100, arquivo);
+		imovel[i].titulo[strlen(imovel[i].titulo) - 1] = '\0';
+
+		if (flag == 1) {//ler informacoes da casa
+			fscanf(arquivo, "%d\n", &imovel[i].casa.pavimentos);
+			fscanf(arquivo, "%d\n", &imovel[i].casa.quartos);
+			fscanf(arquivo, "%.1f\n", &imovel[i].casa.areaTerreno);
+			fscanf(arquivo, "%.1f\n", &imovel[i].casa.areaConst);
+		}
+		else if (flag == 2) {//ler informacoes do apartamento
+			fgets(imovel[i].apartamento.posicao, 100, arquivo);
+			imovel[i].apartamento.posicao[strlen(imovel[i].apartamento.posicao) - 1] = '\0';
+			fscanf(arquivo, "%d\n", &imovel[i].apartamento.quartos);
+			fscanf(arquivo, "%f\n", &imovel[i].apartamento.area);
+			fscanf(arquivo, "%d\n", &imovel[i].apartamento.andar);
+			fscanf(arquivo, "%.1f\n", &imovel[i].apartamento.valorCond);
+			fscanf(arquivo, "%d\n", &imovel[i].apartamento.garagem);
+		}
+		else if (flag == 3) {//ler informacoes do terreno
+			fscanf(arquivo, "%.1f\n", &imovel[i].terreno.area);
+		}
+	}
+
+	fclose(arquivo);
+}
+
 //Salva as informacoes do imovel no vetor imovel e faz um arquivo .txt
 void salvaArquivoImovel(int flag) {
 	FILE* arquivo;
